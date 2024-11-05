815a36e0e45d9cd600319451462325d585e30eea
quickish007 <140751049+quickish007@users.noreply.github.com>
2024-11-05 01:56:10 -0500

    Peace Out
    "The App is working!! if this doesn’t work, I'm changing careers !! Don't Laugh I can see your smile and big teeth :))"

---
+++ app.py
@@ -0,0 +1,64 @@
+# import pandas as pd
+# import streamlit as st
+# import pickle
+#
+# def recommend(movie):
+#     movie_index = movies[movies['title'] == movie].index[0]
+#     distances = similarity[movie_index]
+#     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+#
+#     recommended_movies = []
+#
+#     for i in movies_list:
+#         recommended_movies.append(movies[i[0]].title)
+#     return recommended_movies
+#
+# movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+# movies = pd.DataFrame(movies_dict)
+# st.title('Movie Recommender')
+#
+# similarity = pickle.load(open('similarity.pkl','rb'))
+#
+# selected_movie_name = st.selectbox(
+#     'How would you like to be connected?',
+#     movies['title'].values
+# )
+#
+# if st.button('Recommend'):
+#     recommendations = recommend(selected_movie_name)
+#     for i in recommendations:
+#         st.write(i)
+#
+
+
+import pandas as pd
+import streamlit as st
+import pickle
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]]['title'])  # Corrected line
+    return recommended_movies
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+st.title('Movie Recommender')
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+selected_movie_name = st.selectbox(
+    'How would you like to be connected?',
+    movies['title'].values
+)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
+
